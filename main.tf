e27c4c3894094f935a8ec4b52741cea601a3ad40
cory.shay <cory.shay@archerirm.com>
2021-11-17 16:34:29 -0600

    Add a public access block for the log_bucket

--- main.tf
+++ main.tf
@@ -106,6 +106,14 @@ resource "aws_s3_bucket" "default" {
 }
 
 # Block direct public access
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 resource "aws_s3_bucket_public_access_block" "default" {
   bucket = aws_s3_bucket.default.id
 
